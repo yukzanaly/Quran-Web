f68405470820202ccb7b1547cb379926d89b23df
Aufani Yukzanali <yukzanaly@rocketmail.com>
2024-09-19 08:57:24 +0700

    Create service-worker.js
    untuk pwa juga

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+elf.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/surat.css',
+                '/surat.html',
+                '/scripts.js',
+                '/surat.js',
+                '/SideSurat.js',
+                '/images/alquran.png',
+                // Add any other assets you want to cache
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
