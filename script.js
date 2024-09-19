2129409004578a3e866453d847916aab0ed1f6ef
Aufani Yukzanali <yukzanaly@rocketmail.com>
2024-09-19 08:59:06 +0700

    Update script.js
    untuk buat pwa

--- script.js
+++ script.js
@@ -330,3 +330,13 @@ function getBookLS() {
     ? JSON.parse(localStorage.getItem("mark"))
     : [];
 }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/service-worker.js')
+        .then((registration) => {
+            console.log('Service Worker registered successfully:', registration);
+        })
+        .catch((error) => {
+            console.log('Service Worker registration failed:', error);
+        });
+}
